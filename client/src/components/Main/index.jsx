90eee687dca59b9b450a68e2f7786fa5b25de87e
Dimuthu Lakmal <info.dlrathnayaka@gmail.com>
2025-05-07 21:56:09 +0530

    complete responsive

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,21 +1,25 @@
 import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 const Main = () => {
-	const handleLogout = () => {
-		localStorage.removeItem("token");
-		window.location.reload();
-	};
+		const handleLogout = () => {
+			localStorage.removeItem("token");
+			window.location.reload();
+		};
 
-	return (
-		<div className={styles.main_container}>
-			<nav className={styles.navbar}>
-				<h1>fakebook</h1>
-				<button className={styles.white_btn} onClick={handleLogout}>
-					Logout
-				</button>
-			</nav>
-		</div>
-	);
-};
+		return (
+			<div className={styles.main_container}>
+				<nav className={styles.navbar}>
+					<h1>Welcome</h1>
+					<button className={styles.white_btn} onClick={handleLogout}>
+						Logout
+					</button>
+				</nav>
+				<p>Hello !</p>
+			</div>
+		);
+	};
 
 export default Main;
